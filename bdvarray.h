d2a3f07b03fd1f8e09fb6b6e31e7df24beb0e7a0
BondarevDV <d.bondarev.86@mail.ru>
2020-05-16 00:24:31 +0200

    + add func for dinamic arr: add, resize, enlarge, shrink

--- bdvarray.h
+++ bdvarray.h
@@ -12,13 +12,13 @@ private:
     int _maxSize;
     TypeElem *_array;
 public:
-    BdvArray(int size, int max_size, TypeElem *iniArr =  NULL);
+    BdvArray(int size = 0, int max_size = 0, TypeElem *iniArr =  NULL);
     BdvArray(const BdvArray<TypeElem> &a){
         *this = a;
-
     }
     BdvArray<TypeElem>& operator = (const BdvArray<TypeElem> &a);
     TypeElem& operator [] (int index);
+    int size();
     void Resize(int delta);
     void Add(const TypeElem &elem);
 private:
@@ -30,14 +30,14 @@ private:
 
 
 template<class TypeElem>
-BdvArray<TypeElem>::BdvArray(int size, int max_size, TypeElem *iniArr)/*:
-    _size(size),
-    _maxSize(max_size)*/
+BdvArray<TypeElem>::BdvArray(int size, int max_size, TypeElem *iniArr)
 {
-    if (size < 0 || size > max_size){
-        throw std::out_of_range("Error constructor: init array");
-    }
     _maxSize = (max_size < size ? size : max_size);
+    _size = size;
+    if (_size < 0 || _size > _maxSize){
+        throw std::out_of_range("Error constructor: init array 2222");
+    }
+
     _array = new TypeElem[_maxSize];
     if (iniArr){
         for (int i = 0; i < size; i++) {
@@ -49,21 +49,69 @@ BdvArray<TypeElem>::BdvArray(int size, int max_size, TypeElem *iniArr)/*:
 template<class TypeElem>
 BdvArray<TypeElem> & BdvArray<TypeElem>::operator = (const BdvArray<TypeElem> &a)
 {
-    _array = newlocale()
-    for (int i = 0; i < _size; i++) {
-         = a[i];
+    _array = new TypeElem[a._maxSize];
+    for (int i = 0; i < a._maxSize; i++) {
+        _array[i] = a[i];
     }
+    return *this;
 }
 
 template<class TypeElem>
 TypeElem& BdvArray<TypeElem>::operator [] (int index)
 {
-    if (index >= _size || index < 0 )
-    {
+    if (index >= _size || index < 0 ){
         throw std::out_of_range("Error [] out_of_range: index ");
     }
     return _array[index];
 
+}
+
+template<class TypeElem>
+int BdvArray<TypeElem>::size()
+{
+    return _size;
+
+}
+
+template<class TypeElem>
+void BdvArray<TypeElem>::Resize(int delta)
+{
+    if (delta > 0){
+        _Enlarge(delta);
+    } else if (delta < 0){
+        _Shrink(delta);
+    }
+}
+
+template<class TypeElem>
+void BdvArray<TypeElem>::Add(const TypeElem &elem)
+{
+    Resize(1);
+    _array[_size - 1] = elem;
+
+}
+
+template<class TypeElem>
+void BdvArray<TypeElem>::_Enlarge(int delta)
+{
+    _size += delta;
+    TypeElem* tmp_array = new TypeElem [_size];
+    for (int i = 0; i < _size - delta; i++) {
+        tmp_array[i] = _array[i];
+    }
+    _array = tmp_array;
+    if ( _size > _maxSize) _maxSize = _size;
+}
+
+template<class TypeElem>
+void BdvArray<TypeElem>::_Shrink(int delta)
+{
+    _size = (delta > _size ? 0 : _size - delta);
+    TypeElem* tmp_array = new TypeElem [_size];
+    for (int i = 0; i < _size; i++) {
+        tmp_array[i] = _array[i];
+    }
+    _array = tmp_array;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,66 @@
 #include <iostream>
+#include <stdio.h>
+#include "bdvarray.h"
+#include <stdio.h>     /* для printf */
+#include <stdlib.h>    /* для exit */
+#include <getopt.h>
 
 
 
-int main()
+int main(int argc, char **argv)
 {
+    int c;
+    BdvArray<int> codeTable;
+    while (1) {
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"add", 1, nullptr, 0},
+            {"delete", 1, nullptr, 0},
+            {"verbose", 0, nullptr, 0},
+            {"create", 1, nullptr, 'c'},
+            {"file", 1, nullptr, 0}
+        };
 
-    std::cout << "Hello World!" << std::endl;
+        c = getopt_long (argc, argv, "", long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch (c) {
+        case 0:
+            printf ("параметр %s", long_options[option_index].name);
+            if (optarg)
+                printf (" с аргументом %s", optarg);
+            printf ("\n");
+            break;
+        default:
+            printf ("?? getopt возвратило код символа 0%o ??\n", c);
+        }
+    }
+
+    if (optind < argc) {
+        printf ("элементы ARGV, не параметры: ");
+        while (optind < argc)
+            printf ("%s ", argv[optind++]);
+        printf ("\n");
+    }
+    exit (0);
+//    BdvArray<int> codeTable;
+//    std::cout << "Hello World!" << std::endl;
+//    std::cout << "size = " << codeTable.size() << std::endl;
+//    int index = 0;
+//    codeTable.Add(1);
+//    codeTable.Add(2);
+//    codeTable.Add(3);
+//    codeTable.Add(4);
+//    std::cout << "size = " << codeTable.size() << std::endl;
+//    auto test = codeTable[0];
+//    printf ("item array index=%d, value=%d\n",index, test);
+//    if (argc > 1){
+//          std::cout << argv[0]<<std::endl;} else {
+//          std::cout << "Not arguments" << std::endl;
+//    }
+//    putchar('\a');  /* здесь подается звуковой сигнал */
+//    printf("Счет закончен");
+
     return 0;
 }
